e61c6ee6f3085ab2a8c67383de31769b72a89d44
Zhans2212 <turzhanova.zhan@gmail.com>
2026-06-22 09:05:18 +0500

    3130 unfinished

---
+++ reports/DIA/3127/rep_dia_3130.py
@@ -0,0 +1,242 @@
+from configparser import ConfigParser
+import xlsxwriter
+import datetime
+from util.logger import log
+import oracledb
+import os.path
+from model.manage_reports import set_status_report
+
+report_name = '3130 - Градация СМД принятого для исчисления выплаты'
+report_code = '3130'
+
+stmt_report = """
+SELECT
+  mzp_from,
+  mzp_to,
+  COUNT(DISTINCT CASE WHEN rfpm_id = '07020101' THEN sipr_id END) CNT01,
+  COUNT(DISTINCT CASE WHEN rfpm_id = '07020102' THEN sipr_id END) CNT02,
+  COUNT(DISTINCT CASE WHEN rfpm_id = '07020103' THEN sipr_id END) CNT03,
+  COUNT(DISTINCT sipr_id) CNT
+FROM(
+SELECT
+  rfpm_id,
+  sipr_id,
+  sfa.sum_avg / sfa.mrzp,
+  CASE
+    WHEN sfa.sum_avg / sfa.mrzp > 0 AND sfa.sum_avg / sfa.mrzp < 1 THEN 0
+    WHEN sfa.sum_avg / sfa.mrzp = 1 THEN 1
+    WHEN sfa.sum_avg / sfa.mrzp > 1 AND sfa.sum_avg / sfa.mrzp <= 2 THEN 1
+    WHEN sfa.sum_avg / sfa.mrzp > 2 AND sfa.sum_avg / sfa.mrzp <= 3 THEN 2
+    WHEN sfa.sum_avg / sfa.mrzp > 3 AND sfa.sum_avg / sfa.mrzp <= 4 THEN 3
+    WHEN sfa.sum_avg / sfa.mrzp > 4 AND sfa.sum_avg / sfa.mrzp <= 5 THEN 4
+    WHEN sfa.sum_avg / sfa.mrzp > 5 AND sfa.sum_avg / sfa.mrzp <= 6 THEN 5
+    WHEN sfa.sum_avg / sfa.mrzp > 6 AND sfa.sum_avg / sfa.mrzp <= 7 THEN 6
+    WHEN sfa.sum_avg / sfa.mrzp > 7 AND sfa.sum_avg / sfa.mrzp <= 8 THEN 7
+    WHEN sfa.sum_avg / sfa.mrzp > 8 AND sfa.sum_avg / sfa.mrzp <= 9 THEN 8
+    WHEN sfa.sum_avg / sfa.mrzp > 9 AND sfa.sum_avg / sfa.mrzp < 10 THEN 9
+    WHEN sfa.sum_avg / sfa.mrzp = 10 THEN 10
+    WHEN sfa.sum_avg / sfa.mrzp > 10 THEN 10
+  END mzp_from,
+  CASE
+    WHEN sfa.sum_avg / sfa.mrzp > 0 AND sfa.sum_avg / sfa.mrzp < 1 THEN 1
+    WHEN sfa.sum_avg / sfa.mrzp = 1 THEN 1
+    WHEN sfa.sum_avg / sfa.mrzp > 1 AND sfa.sum_avg / sfa.mrzp <= 2 THEN 2
+    WHEN sfa.sum_avg / sfa.mrzp > 2 AND sfa.sum_avg / sfa.mrzp <= 3 THEN 3
+    WHEN sfa.sum_avg / sfa.mrzp > 3 AND sfa.sum_avg / sfa.mrzp <= 4 THEN 4
+    WHEN sfa.sum_avg / sfa.mrzp > 4 AND sfa.sum_avg / sfa.mrzp <= 5 THEN 5
+    WHEN sfa.sum_avg / sfa.mrzp > 5 AND sfa.sum_avg / sfa.mrzp <= 6 THEN 6
+    WHEN sfa.sum_avg / sfa.mrzp > 6 AND sfa.sum_avg / sfa.mrzp <= 7 THEN 7
+    WHEN sfa.sum_avg / sfa.mrzp > 7 AND sfa.sum_avg / sfa.mrzp <= 8 THEN 8
+    WHEN sfa.sum_avg / sfa.mrzp > 8 AND sfa.sum_avg / sfa.mrzp <= 9 THEN 9
+    WHEN sfa.sum_avg / sfa.mrzp > 9 AND sfa.sum_avg / sfa.mrzp < 10 THEN 10
+    WHEN sfa.sum_avg / sfa.mrzp = 10 THEN 10
+    WHEN sfa.sum_avg / sfa.mrzp > 10 THEN NULL
+  END mzp_to
+  FROM SIPR_MAKET_FIRST_APPROVE_2 sfa
+ WHERE RFPM_ID LIKE '0702%'
+   AND TRUNC(DATE_APPROVE) BETWEEN TO_DATE(:dt_from,'YYYY-MM-DD') AND TO_DATE(:dt_to,'YYYY-MM-DD')
+) 
+GROUP BY mzp_from, mzp_to
+ORDER BY mzp_from, mzp_to
+"""
+
+
+def format_worksheet(worksheet, common_format):
+
+    worksheet.set_row(0, 24)
+    worksheet.set_row(1, 24)
+    worksheet.set_row(2, 42)
+
+    worksheet.set_column(0, 1, 12)   # MZP от
+    worksheet.set_column(1, 2, 12)   # MZP до
+    worksheet.set_column(2, 6, 20)
+
+    headers = [
+        'МЗП от',
+        'МЗП до',
+        'Степень утраты трудоспособности 80-100%',
+        'Степень утраты трудоспособности 60-80%',
+        'Степень утраты трудоспособности 30-60%',
+        'Итого, человек'
+    ]
+
+    for col, header in enumerate(headers):
+        worksheet.write(2, col, header, common_format)
+
+
+def do_report(file_name: str, date_first: str, date_second: str):
+    if os.path.isfile(file_name):
+        log.info(f'Отчет уже существует {file_name}')
+        return file_name
+
+    s_date = datetime.datetime.now().strftime("%H:%M:%S")
+
+    log.info(f'DO REPORT. START {report_code}. DATE_FROM: {date_first}, FILE_PATH: {file_name}')
+
+    config = ConfigParser()
+    config.read('db_config.ini')
+
+    ora_config = config['rep_db_loader']
+    db_user = ora_config['db_user']
+    db_password = ora_config['db_password']
+    db_dsn = ora_config['db_dsn']
+    log.info(f'{report_code}. db_user: {db_user}, db_dsn: {db_dsn}')
+
+    with oracledb.connect(user=db_user, password=db_password, dsn=db_dsn) as connection:
+        with connection.cursor() as cursor:
+            workbook = xlsxwriter.Workbook(file_name)
+
+            title_format = workbook.add_format({'bg_color': '#D1FFFF', 'align': 'center', 'font_color': 'black'})
+            # title_format = workbook.add_format({'bg_color': '#C5FFFF', 'align': 'center', 'font_color': 'black'})
+            title_format.set_align('vcenter')
+            title_format.set_border(1)
+            title_format.set_text_wrap()
+            title_format.set_bold()
+
+            title_name_report = workbook.add_format({'align': 'left', 'font_color': 'black', 'font_size': '14'})
+            title_name_report.set_align('vcenter')
+            title_name_report.set_bold()
+
+            title_format_it = workbook.add_format({'align': 'right'})
+            title_format_it.set_align('vcenter')
+            title_format_it.set_italic()
+
+            title_report_code = workbook.add_format({'align': 'right', 'font_size': '14'})
+            title_report_code.set_align('vcenter')
+            title_report_code.set_bold()
+
+            common_format = workbook.add_format({'align': 'center', 'font_color': 'black'})
+            common_format.set_align('vcenter')
+            common_format.set_border(1)
+
+            region_name_format = workbook.add_format({'align': 'left', 'font_color': 'black'})
+            region_name_format.set_align('vcenter')
+            region_name_format.set_border(1)
+
+            sum_pay_format = workbook.add_format(
+                {'num_format': '#,###,##0.00', 'font_color': 'black', 'align': 'vcenter'})
+            sum_pay_format.set_border(1)
+
+            date_format = workbook.add_format({'num_format': 'dd.mm.yyyy', 'align': 'center'})
+            date_format.set_border(1)
+            date_format.set_align('vcenter')
+
+            digital_format = workbook.add_format({'num_format': '#0', 'align': 'center'})
+            digital_format.set_border(1)
+            digital_format.set_align('vcenter')
+
+            money_format = workbook.add_format({'num_format': '# ### ### ##0.00', 'align': 'right'})
+            money_format.set_border(1)
+            money_format.set_align('vcenter')
+
+            now = datetime.datetime.now()
+            log.info(f'Начало формирования {file_name}: {now.strftime("%d-%m-%Y %H:%M:%S")}')
+            page_num = 1
+            worksheet = []
+            worksheet.append(workbook.add_worksheet(f'Список {page_num}'))
+            sql_sheet = workbook.add_worksheet('SQL')
+            merge_format = workbook.add_format({
+                'bold': False,
+                'border': 6,
+                'align': 'left',
+                'valign': 'vcenter',
+                'fg_color': '#FAFAD7',
+                'text_wrap': True
+            })
+            sql_sheet.merge_range(f'A1:I{len(stmt_report.splitlines())}', f'{stmt_report}', merge_format)
+
+            worksheet[page_num - 1].activate()
+            format_worksheet(worksheet=worksheet[page_num - 1], common_format=title_format)
+
+            worksheet[page_num - 1].write(0, 0, report_name, title_name_report)
+            worksheet[page_num - 1].write(1, 0, f'За период: {date_first} - {date_second}', title_name_report)
+
+            log.info(f'REPORT {report_code}. CREATING REPORT')
+
+            try:
+                cursor.execute(stmt_report, dt_from=date_first, dt_to=date_second)
+            except oracledb.DatabaseError as e:
+                error, = e.args
+                log.error(f"ERROR. REPORT {report_code}. error_code: {error.code}, error: {error.message}")
+                log.info(f'\n---------\n{stmt_report}\n---------')
+                set_status_report(file_name, 3)
+                return None
+            finally:
+                log.info(f'REPORT: {report_code}. Выборка из курсора завершена')
+
+            log.info(f'REPORT: {report_code}. Формируем выходную EXCEL таблицу')
+
+            rows = cursor.fetchall()
+
+            if not rows:
+                log.warning(f'REPORT {report_code}. Данные отсутствуют')
+                workbook.close()
+                set_status_report(file_name, 2)
+                return None
+
+            all_cnt = len(rows)
+
+            row_num = 3
+
+            for idx, record in enumerate(rows, start=1):
+                worksheet[0].write(row_num, 0, record[0], digital_format)
+                worksheet[0].write(row_num, 1, record[1], digital_format)
+
+                worksheet[0].write(row_num, 2, record[2], digital_format)
+                worksheet[0].write(row_num, 3, record[3], digital_format)
+                worksheet[0].write(row_num, 4, record[4], digital_format)
+                worksheet[0].write(row_num, 5, record[5], digital_format)
+
+                row_num += 1
+
+            worksheet[0].freeze_panes(3, 0)
+            worksheet[0].freeze_panes(4, 0)
+            worksheet[0].freeze_panes(5, 0)
+
+            now = datetime.datetime.now()
+            stop_time = now.strftime("%H:%M:%S")
+
+            for i in range(page_num):
+                # Шифр отчета
+                worksheet[i].write(0, 5, report_code, title_report_code)
+                worksheet[i].write(1, 5, f'Дата формирования: {now.strftime("%d.%m.%Y ")}({s_date} - {stop_time})',
+                                   title_format_it)
+
+            workbook.close()
+            set_status_report(file_name, 2)
+
+            log.info(
+                f'REPORT: {report_code}. Формирование отчета {file_name} завершено ({s_date} - {stop_time}). Загружено {all_cnt} записей')
+
+
+def thread_report(file_name: str, date_first: str, date_second: str):
+    import threading
+    log.info(f'THREAD REPORT. {datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")} -> {file_name}')
+    log.info(f'THREAD REPORT. PARAMS: date_from: {date_first}')
+    threading.Thread(target=do_report, args=(file_name, date_first, date_second), daemon=True).start()
+    return {"status": 1, "file_path": file_name}
+
+
+if __name__ == "__main__":
+    log.info(f'Отчет {report_code} запускается.')
+    do_report('minSO_01.xlsx', '01.10.2022', '31.10.2022')
