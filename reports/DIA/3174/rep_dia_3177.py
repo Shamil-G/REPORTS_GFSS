731e697a194fa350bdc4b0e6827d0e2bcef3961f
Zhans2212 <turzhanova.zhan@gmail.com>
2026-07-13 10:26:31 +0500

    3177 3178 3179 added

---
+++ reports/DIA/3174/rep_dia_3177.py
@@ -0,0 +1,250 @@
+from configparser import ConfigParser
+import xlsxwriter
+import datetime
+
+from xlsxwriter.utility import xl_col_to_name
+
+from util.logger import log
+import oracledb
+import os.path
+from model.manage_reports import set_status_report
+
+report_name = '3177 - Градация размеров получателей'
+report_code = '3177'
+
+stmt_report = """
+SELECT
+  sum_from,
+  sum_to,
+  COUNT(DISTINCT CASE WHEN rfpm in ('07050001', '07050101') THEN pncd_id END) CNT01, 
+  COUNT(DISTINCT CASE WHEN rfpm in ('07050002', '07050102') THEN pncd_id END) CNT02,
+  COUNT(DISTINCT CASE WHEN rfpm in ('07050003', '07050103') THEN pncd_id END) CNT03,
+  COUNT(DISTINCT CASE WHEN rfpm in ('07050004', '07050104') THEN pncd_id END) CNT04,
+  COUNT(DISTINCT pncd_id) CNT
+FROM(
+SELECT
+  FIRST_VALUE(D.RFPM_ID) OVER(PARTITION BY D.pncd_id ORDER BY D.PNCP_DATE DESC) RFPM,
+  d.pncd_id,
+  d.source_id,
+  ph.sum_pay,
+  TRUNC(ph.sum_pay / 1000) * 1000 sum_from,
+  (TRUNC(ph.sum_pay / 1000) + 1) * 1000 sum_to
+  FROM PNPD_DOCUMENT D, PAYMENT_HISTORY PH
+ WHERE D.PNPD_ID = PH.PNPD_ID
+   AND D.PNCP_DATE BETWEEN TO_DATE(:dt_from,'YYYY-MM-DD') AND TO_DATE(:dt_to,'YYYY-MM-DD')
+   AND D.RFPM_ID LIKE '0705%'
+   AND D.RIDT_ID IN (4, 6, 7, 8)
+   AND D.STATUS IN (0, 1, 2, 3, 5, 7)
+   AND D.PNSP_ID > 0)
+GROUP BY sum_from, sum_to
+ORDER BY sum_from, sum_to
+"""
+
+
+def format_worksheet(worksheet, common_format):
+
+    worksheet.set_row(0, 24)
+    worksheet.set_row(1, 24)
+    worksheet.set_row(2, 24)
+    worksheet.set_row(3, 42)
+
+    worksheet.set_column(0, 1, 12)
+    worksheet.set_column(2, 7, 15)
+
+    worksheet.merge_range(2, 0, 2, 1, 'Градация,\nтг', common_format)
+    worksheet.merge_range(
+        2, 2, 2, 5,
+        'По уходу за',
+        common_format
+    )
+    worksheet.merge_range(2, 6, 3, 6, 'Итого,\nчеловек', common_format)
+
+    # Подзаголовки
+    worksheet.write(3, 0, 'От', common_format)
+    worksheet.write(3, 1, 'До', common_format)
+
+    worksheet.write(3, 2, 'Первым ребенком', common_format)
+    worksheet.write(3, 3, 'Вторым ребенком', common_format)
+    worksheet.write(3, 4, 'Третьим ребенком', common_format)
+    worksheet.write(3, 5, 'Четвертым и более ребенком', common_format)
+
+def do_report(file_name: str, date_first: str, date_second: str):
+    if os.path.isfile(file_name):
+        log.info(f'Отчет уже существует {file_name}')
+        return file_name
+
+    s_date = datetime.datetime.now().strftime("%H:%M:%S")
+
+    log.info(f'DO REPORT. START {report_code}. DATE_FROM: {date_first}, FILE_PATH: {file_name}')
+
+    config = ConfigParser()
+    config.read('db_config.ini')
+
+    ora_config = config['rep_db_loader']
+    db_user = ora_config['db_user']
+    db_password = ora_config['db_password']
+    db_dsn = ora_config['db_dsn']
+    log.info(f'{report_code}. db_user: {db_user}, db_dsn: {db_dsn}')
+
+    with oracledb.connect(user=db_user, password=db_password, dsn=db_dsn) as connection:
+        with connection.cursor() as cursor:
+            workbook = xlsxwriter.Workbook(file_name)
+
+            title_format = workbook.add_format({'bg_color': '#D1FFFF', 'align': 'center', 'font_color': 'black'})
+            # title_format = workbook.add_format({'bg_color': '#C5FFFF', 'align': 'center', 'font_color': 'black'})
+            title_format.set_align('vcenter')
+            title_format.set_border(1)
+            title_format.set_text_wrap()
+            title_format.set_bold()
+
+            title_name_report = workbook.add_format({'align': 'left', 'font_color': 'black', 'font_size': '14'})
+            title_name_report.set_align('vcenter')
+            title_name_report.set_bold()
+
+            title_format_it = workbook.add_format({'align': 'right'})
+            title_format_it.set_align('vcenter')
+            title_format_it.set_italic()
+
+            title_report_code = workbook.add_format({'align': 'right', 'font_size': '14'})
+            title_report_code.set_align('vcenter')
+            title_report_code.set_bold()
+
+            common_format = workbook.add_format({'align': 'center', 'font_color': 'black'})
+            common_format.set_align('vcenter')
+            common_format.set_border(1)
+
+            region_name_format = workbook.add_format({'align': 'left', 'font_color': 'black'})
+            region_name_format.set_align('vcenter')
+            region_name_format.set_border(1)
+
+            sum_pay_format = workbook.add_format(
+                {'num_format': '#,###,##0.00', 'font_color': 'black', 'align': 'vcenter'})
+            sum_pay_format.set_border(1)
+
+            date_format = workbook.add_format({'num_format': 'dd.mm.yyyy', 'align': 'center'})
+            date_format.set_border(1)
+            date_format.set_align('vcenter')
+
+            digital_format = workbook.add_format({'num_format': '#0', 'align': 'center'})
+            digital_format.set_border(1)
+            digital_format.set_align('vcenter')
+
+            total_digital_format = workbook.add_format({'num_format': '#0', 'align': 'center'})
+            total_digital_format.set_border(1)
+            total_digital_format.set_align('vcenter')
+            total_digital_format.set_bold()
+
+            money_format = workbook.add_format({'num_format': '# ### ### ##0.00', 'align': 'right'})
+            money_format.set_border(1)
+            money_format.set_align('vcenter')
+
+            total_money_format = workbook.add_format({'num_format': '# ### ### ##0.00', 'align': 'right'})
+            total_money_format.set_border(1)
+            total_money_format.set_align('vcenter')
+            total_money_format.set_bold()
+
+            now = datetime.datetime.now()
+            log.info(f'Начало формирования {file_name}: {now.strftime("%d-%m-%Y %H:%M:%S")}')
+            page_num = 1
+            worksheet = []
+            worksheet.append(workbook.add_worksheet(f'Список {page_num}'))
+            sql_sheet = workbook.add_worksheet('SQL')
+            merge_format = workbook.add_format({
+                'bold': False,
+                'border': 6,
+                'align': 'left',
+                'valign': 'vcenter',
+                'fg_color': '#FAFAD7',
+                'text_wrap': True
+            })
+            sql_sheet.merge_range(f'A1:I{len(stmt_report.splitlines())}', f'{stmt_report}', merge_format)
+
+            worksheet[page_num - 1].activate()
+            format_worksheet(worksheet=worksheet[page_num - 1], common_format=title_format)
+
+            worksheet[page_num - 1].write(0, 0, report_name, title_name_report)
+            worksheet[page_num - 1].write(1, 0, f'За период: {date_first} - {date_second}', title_name_report)
+
+            log.info(f'REPORT {report_code}. CREATING REPORT')
+
+            try:
+                cursor.execute(stmt_report, dt_from=date_first, dt_to=date_second)
+            except oracledb.DatabaseError as e:
+                error, = e.args
+                log.error(f"ERROR. REPORT {report_code}. error_code: {error.code}, error: {error.message}")
+                log.info(f'\n---------\n{stmt_report}\n---------')
+                set_status_report(file_name, 3)
+                return None
+            finally:
+                log.info(f'REPORT: {report_code}. Выборка из курсора завершена')
+
+            log.info(f'REPORT: {report_code}. Формируем выходную EXCEL таблицу')
+
+            rows = cursor.fetchall()
+
+            if not rows:
+                log.warning(f'REPORT {report_code}. Данные отсутствуют')
+                workbook.close()
+                set_status_report(file_name, 2)
+                return None
+
+            all_cnt = len(rows)
+
+            first_row = 5
+            row_num = first_row - 1
+
+            for record in rows:
+                worksheet[0].write(row_num, 0, record[0], digital_format)
+                worksheet[0].write(row_num, 1, record[1], digital_format)
+
+                worksheet[0].write(row_num, 2, record[2], digital_format)
+                worksheet[0].write(row_num, 3, record[3], digital_format)
+                worksheet[0].write(row_num, 4, record[4], digital_format)
+                worksheet[0].write(row_num, 5, record[5], digital_format)
+                worksheet[0].write(row_num, 6, record[6], digital_format)
+
+                row_num += 1
+
+            # строка итогов
+            worksheet[0].merge_range(row_num, 0, row_num, 1, 'ИТОГО', title_format)
+
+            for col in range(2, 7):
+                col_letter = xl_col_to_name(col)
+
+                worksheet[0].write_formula(
+                    row_num,
+                    col,
+                    f'=SUM({col_letter}{first_row}:{col_letter}{row_num})',
+                    total_digital_format
+                )
+
+            worksheet[0].freeze_panes(3, 0)
+            worksheet[0].freeze_panes(4, 0)
+
+            now = datetime.datetime.now()
+            stop_time = now.strftime("%H:%M:%S")
+
+            for i in range(page_num):
+                # Шифр отчета
+                worksheet[i].write(0, 5, report_code, title_report_code)
+                worksheet[i].write(1, 5, f'Дата формирования: {now.strftime("%d.%m.%Y ")}({s_date} - {stop_time})',
+                                   title_format_it)
+
+            workbook.close()
+            set_status_report(file_name, 2)
+
+            log.info(
+                f'REPORT: {report_code}. Формирование отчета {file_name} завершено ({s_date} - {stop_time}). Загружено {all_cnt} записей')
+
+
+def thread_report(file_name: str, date_first: str, date_second: str):
+    import threading
+    log.info(f'THREAD REPORT. {datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")} -> {file_name}')
+    log.info(f'THREAD REPORT. PARAMS: date_from: {date_first}')
+    threading.Thread(target=do_report, args=(file_name, date_first, date_second), daemon=True).start()
+    return {"status": 1, "file_path": file_name}
+
+
+if __name__ == "__main__":
+    log.info(f'Отчет {report_code} запускается.')
+    do_report('minSO_01.xlsx', '01.10.2022', '31.10.2022')
